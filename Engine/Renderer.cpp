1c79c908019e2fd3414c3be4cbe020f03b1463e4
JDCalvert <jd.calvert@hotmail.co.uk>
2018-04-24 22:21:02 +0100

    Done some more

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -62,5 +62,10 @@ void Renderer::initialiseFrame()
     glViewport(0, 0, width, height);
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Renderer::renderFrame()
+{
+    screenShader->render(frameTexture);
 }
